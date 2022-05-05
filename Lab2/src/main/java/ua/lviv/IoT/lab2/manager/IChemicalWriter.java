8fad4962d913746817b48ec770d47682a770583d
pavlo <pavlo.to474@gmail.com>
2022-05-06 01:52:31 +0300

    add try-with-resources after defend of lab work

--- Lab2/src/main/java/ua/lviv/IoT/lab2/manager/IChemicalWriter.java
+++ Lab2/src/main/java/ua/lviv/IoT/lab2/manager/IChemicalWriter.java
@@ -2,9 +2,10 @@ package ua.lviv.IoT.lab2.manager;
 
 import ua.lviv.IoT.lab2.model.Chemical;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IChemicalWriter {
 
-   void writeToFile(List<Chemical> chemicals);
+   void writeToFile(List<Chemical> chemicals) throws IOException;
 }
